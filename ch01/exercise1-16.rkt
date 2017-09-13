e1defea3227ae680a72a0ad74fc225654346fd69
Yan Zujia <yanzujia@gmail.com>
2017-09-13 11:03:16 +0800

    add 1-16

---
+++ ch01/exercise1-16.rkt
@@ -0,0 +1,13 @@
+(define (fast-expt b n)
+  (fast-iter b n 1))
+(define (fast-iter b counter product)
+  (if (= counter 0)
+      product
+      (if (even? counter)
+          (fast-iter (square b) (/ counter 2) product)
+          (fast-iter b (- counter 1) (* b product)))))
+(define (even? n)
+  (= (remainder n 2) 0))
+(define (square x)
+  (* x x))
+(fast-expt 5 10000)
