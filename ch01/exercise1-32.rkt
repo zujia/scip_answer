04fd6d61faed837d885888294ea87392c78874c0
Yan Zujia <yanzujia@gmail.com>
2017-09-14 15:10:40 +0800

    add 1-32

---
+++ ch01/exercise1-32.rkt
@@ -0,0 +1,20 @@
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result
+                                 (term a)))))
+  (iter a null-value))
+(define (sum term a next b)
+  (accumulate-iter + 0 term a next b))
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+(define (cube n) (* n n n))
+(sum-cubes 1 10)
+
