79170e01e0eb81f85eca9fd0cf42f82ca9536a59
Yan Zujia <yanzujia@gmail.com>
2017-10-08 21:52:24 +0800

    modified 2-31

--- ch02/exercise2-31.rkt
+++ ch02/exercise2-31.rkt
@@ -3,6 +3,7 @@
         ((not (pair? tree)) (proc tree))
         (else (cons (tree-map proc (car tree))
                     (tree-map proc (cdr tree))))))
+(define (square x) (* x x))
 (define (square-tree tree)
   (tree-map square tree))
 (square-tree
