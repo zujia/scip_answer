0177cc477d51856ad538c6338d8f36b0cf2675fd
Yan Zujia <yanzujia@gmail.com>
2018-03-07 12:17:25 +0800

    add 2-46,47,48,49

---
+++ ch02/exercise2-49.rkt
@@ -0,0 +1,41 @@
+#lang sicp
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (sement)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+(define (draw-outline frame)
+  ((segments->painter
+    (list (make-segment (make-vect 0 0)
+                        (make-vect 0 1))
+          (make-segment (make-vect 0 1)
+                        (make-vect 1 1))
+          (make-segment (make-vect 1 1)
+                        (make-vect 1 0))
+          (make-segment (make-vect 1 0)
+                        (make-vect 0 0))))
+   frame))
+
+(define (draw-x frame)
+  ((segments->painter
+    (list (make-segment (make-vect 0 0)
+                        (make-vect 1 1))
+          (make-segment (make-vect 0 1)
+                        (make-vect 1 0))))
+   frame))
+
+(define (draw-diamond frame)
+  ((segments->painter
+    (list (make-segment (make-vect 0 0.5)
+                        (make-vect 0.5 1))
+          (make-segment (make-vect 0.5 1)
+                        (make-vect 1 0.5))
+          (make-segment (make-vect 1 0.5)
+                        (make-vect 0.5 0))
+          (make-segment (make-vect 0.5 0)
+                        (make-vect 0 0.5))))
+   frame))
